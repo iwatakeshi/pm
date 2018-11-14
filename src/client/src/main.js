171625f2c0a00698e382172f2c2c87901d03cfeb
Takeshi <iwatakeshi@gmail.com>
2018-11-13 21:06:47 -0700

    Add Home.vue
    Added router and routes for the view.

--- src/client/src/main.js
+++ src/client/src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// Add Vue Router
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: require("./pages/Home") },
+  // { path: '/project/:id', component: require('pages/Project') },
+  // { path: '/task/:id', component: require('pages/Task') }
+]
+
+const router = new VueRouter({ routes })
+
+
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/client/src/pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="row">
+        <p>Hello</p>
+        <ul v-if="projects">
+            <li v-for="project in projects">{{ project.title }}</li>                        
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "home",
+    data: () => {
+        projects: null      
+    },
+    beforeMount() {
+        this.getProjects();   
+    },
+    methods: {
+        getProjects() {
+            this.projects = []
+        }
+    }
+}
+</script>
+
+<style></style>
